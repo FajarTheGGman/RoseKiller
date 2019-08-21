9eca814aca8d9f7caa70c852a862bd2448c65b70
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-08-21 17:34:19 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from bs4 import BeautifulSoup as bs
+import urllib3 as url
+from content.xss import *
+
+class Main:
+    def banner():
+        print("               ';.")
+        print("    .---.,       \  [!] Report Error To My Social Media :)")
+        print("   []-.__,>=======;==================")
+        print("    `----'      ,/   [Rose Killer]")
+        print("              .;'        [By]")
+        print("                    [Fajar Firdaus]")
+    def Run():
+        def help():
+            print("[Help Commands]")
+            print("- help (See all commands)")
+            print("- xss (exploit websites using xss method)")
+            print("- dork (dork website)")
+            print("- script (take script deface in website)")
+        
+        user = str(input("\n\n[RoseKiller] >_ "))
+        if user == "xss":
+            xss = Xss.main()
+        elif user == "dork":
+            dork = Dork.main()
+        elif user == "script":
+            sc = Sc.main()
+        elif user == "help":
+            help()
+        else:
+            print("[!] Wrong Commands")
+            help()
+
+banner = Main.banner()
+r = Main.Run()
