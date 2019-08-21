b9b0bde271af6cdf4bbbb38e55c4098ca239477d
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-08-21 17:47:09 +0700

    Update dork.py

--- content/dork.py
+++ content/dork.py
@@ -5,9 +5,8 @@ class Dork:
     def main():
         req = url.PoolManager()
         user = str(input("[Dork] >_ "))
-        for ulang in range(10):
-            send = req.request("GET", "http://www1.search-results.com/web?q=" + user + "&page=" + str(ulang))
-            parsing = BeautifulSoup(send, features="lxml")
-            print("[+] Here's The Results")
-            for data in parsing.find_all("cite"):
-                print(data.string)
+        print("[+] Here's The Results")
+        send = req.request("GET", "http://www1.search-results.com/web?q=" + user)
+        parsing = BeautifulSoup(send.data, features="lxml")
+        for data in parsing.find_all("cite"):
+            print(data.string)
